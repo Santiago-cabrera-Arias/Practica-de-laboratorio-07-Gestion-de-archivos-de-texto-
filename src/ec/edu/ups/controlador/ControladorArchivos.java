906df9e78c35043465f36b13c65f3e9a93854e80
Santiago-cabrera-Arias <santiagocabrera354@gmail.com>
2020-07-11 23:26:30 -0500

    Creacion del metodo archivoTexto.

--- src/ec/edu/ups/controlador/ControladorArchivos.java
+++ src/ec/edu/ups/controlador/ControladorArchivos.java
@@ -5,6 +5,11 @@
  */
 package ec.edu.ups.controlador;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,7 +21,7 @@ public class ControladorArchivos {
 
     //Atributos de tipo String
     private String ruta;
-    private String archivos;
+    private File archivos;
 
     //Utilizamos el map debido a que este no puede contener llaves duplicadas.
     private Map<String, String> texto;
@@ -74,12 +79,35 @@ public class ControladorArchivos {
     public void diccionarioabecedario() {
         String[] abecedario = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
             "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
-        
+
         String[] abecedarioEncriptado = {"~", "!", "@", "#", "$", "5", "6", "%", "^", "&", "*",
             "8", "|", "]", "`", "c", "a", "r", ">", "<", "?", "/", ".", "_", "-", "h"};
 
     }
-    
-    
+
+    public void archivoTexto(String rutas, String nombre, String texto) {
+
+        try {
+            archivos = new File(rutas + File.separator + nombre);
+            //el getAbsoltePath nos proporciona la ruta absoluta de un fichero.
+            String rutaC = archivos.getAbsolutePath();
+            //FileWriter nos sirve para escribir caracteres en archivos.
+            FileWriter archivoE = new FileWriter(rutaC, true);
+            //El BufferedWriter tiene la misma funcionalidad que el FileWriter  
+            BufferedWriter caracteresArchivo = new BufferedWriter(archivoE);
+            //agrega los caracteres al final del builder 
+            caracteresArchivo.append(texto);
+            caracteresArchivo.close();
+
+            //En caso de que se produzca una excepcion el programa es interrumpido y terminado.
+        } catch (FileNotFoundException e) {
+
+        } catch (IOException e1) {
+
+        } catch (Exception e2) {
+
+        }
+
+    }
 
 }
